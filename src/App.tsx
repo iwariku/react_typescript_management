160e28c1e5881a2ba447f5bf5208b0ce9964e95e
iwariku <02rikuto18@gmail.com>
2026-03-21 22:43:13 +0900

    feat: チェックボックスの切り替えるとユーザー情報も切り替わる機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { UserList } from './User';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 function App() {
   return (
     <>
-      <h2>コードとファイルの削除</h2>
+      <UserList />
     </>
   );
 }
