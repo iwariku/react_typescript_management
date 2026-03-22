f3b253cae5a21d63d4a3e42b81c344f7688722c2
iwariku <02rikuto18@gmail.com>
2026-03-22 16:01:12 +0900

    feat: 全員のタブで、生徒とメンターだけが持つ情報も表示

--- src/components/AllUserList.tsx
+++ src/components/AllUserList.tsx
@@ -1,4 +1,5 @@
 import type { AllUser } from '../types';
+import { Mentor, Student } from '../types';
 
 type Props = {
   allUsers: AllUser[];
@@ -18,6 +19,18 @@ export const AllUserList = ({ allUsers }: Props) => {
             <th scope="col">電話番号</th>
             <th scope="col">趣味</th>
             <th scope="col">URL</th>
+
+            <th scope="col">勉強時間</th>
+            <th scope="col">課題番号</th>
+            <th scope="col">勉強中の言語</th>
+            <th scope="col">ハピネススコア</th>
+            <th scope="col">対応可能なメンター</th>
+
+            <th scope="col">実務経験年数</th>
+            <th scope="col">現場で使っている言語</th>
+            <th scope="col">担当できる課題番号の始め</th>
+            <th scope="col">担当できる課題番号の終わり</th>
+            <th scope="col">対応可能な生徒</th>
           </tr>
         </thead>
         <tbody>
@@ -31,6 +44,22 @@ export const AllUserList = ({ allUsers }: Props) => {
               <td>{user.phone}</td>
               <td>{user.hobbies}</td>
               <td>{user.url}</td>
+
+              <td>{user instanceof Student ? `${user.studyMinutes}` : ''}</td>
+              <td>{user instanceof Student ? `${user.taskCode}` : ''}</td>
+              <td>{user instanceof Student ? `${user.studyLangs}` : ''}</td>
+              <td>{user instanceof Student ? `${user.score}` : ''}</td>
+              <td>生徒仮</td>
+
+              <td>{user instanceof Mentor ? `${user.experienceDays}` : ''}</td>
+              <td>{user instanceof Mentor ? `${user.useLangs}` : ''}</td>
+              <td>
+                {user instanceof Mentor ? `${user.availableStartCode}` : ''}
+              </td>
+              <td>
+                {user instanceof Mentor ? `${user.availableEndCode}` : ''}
+              </td>
+              <td>メンター仮</td>
             </tr>
           ))}
         </tbody>
