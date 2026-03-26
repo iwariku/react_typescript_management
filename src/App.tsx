79d177df481a7b10c696603d656f5deb9389f02c
iwariku <02rikuto18@gmail.com>
2026-03-26 11:00:01 +0900

    refactor: AppのロジックをuseAppフックに切り出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
-import { useState } from 'react';
 import { CreateUser } from './components/form/CreateUser';
 import { UserTabs } from './components/list/UserTabs';
-
-import { allInstances } from './data';
-import type { AllUser, Mentor, Student } from './types/User';
+import { useApp } from './hooks/useApp';
 
 function App() {
-  // データはAppで保持する -> 一番上でデータを保持する
-  // すべてのトップになるので、共通の親クラスであるAllUser型にする
-  const [users, setUsers] = useState<AllUser[]>(allInstances);
-
-  //
-  const handleAddUser = (newUser: Student | Mentor) => {
-    console.log('3. App.tsx: 親にデータが届きました', newUser);
-    setUsers([...users, newUser]);
-  };
-
-  console.log('4. App.tsx: 現在の全ユーザー数', users.length);
+  const { users, handleAddUser } = useApp();
 
   return (
     <>
---
+++ src/hooks/useApp.ts
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { allInstances } from '../data';
+import type { AllUser, Mentor, Student } from '../types/User';
+
+export const useApp = () => {
+  // データはAppで保持する -> 一番上でデータを保持する
+  // すべてのトップになるので、共通の親クラスであるAllUser型にする
+  const [users, setUsers] = useState<AllUser[]>(allInstances);
+
+  //
+  const handleAddUser = (newUser: Student | Mentor) => {
+    setUsers([...users, newUser]);
+  };
+
+  return {
+    users,
+    handleAddUser,
+  };
+};
