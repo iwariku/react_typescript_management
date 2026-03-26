eda8fa5fe483c53c7c54b094582e234b53dab98b
iwariku <02rikuto18@gmail.com>
2026-03-26 11:31:54 +0900

    refactor: StudentListのロジックをuseStudentSortフックに切り出し

--- src/components/list/StudentList.tsx
+++ src/components/list/StudentList.tsx
@@ -1,47 +1,14 @@
-import { useState } from 'react';
 import { type Student } from '../../types/User';
+import { useStudentSort } from '../../hooks/useStudentSort';
 
 type Props = {
   students: Student[];
 };
 
-type SortKey = 'studyMinutes' | 'score' | undefined;
-type SortOrder = 'asc' | 'desc' | undefined;
-
 export const StudentList = ({ students }: Props) => {
-  const [sortKey, setSortKey] = useState<SortKey>(undefined);
-  const [sortOrder, setSortOrder] = useState<SortOrder>(undefined);
-
-  // ソートロジック
-  const sortedStudents = [...students].sort((a, b) => {
-    if (!sortKey || !sortOrder) return a.id - b.id;
-
-    // sortKeyによってどちらの項目を選択しているかを判断する
-    const valA = sortKey === 'studyMinutes' ? a.studyMinutes : a.score;
-    const valB = sortKey === 'studyMinutes' ? b.studyMinutes : b.score;
-
-    return sortOrder === 'asc' ? valA - valB : valB - valA;
+  const { sortKey, sortOrder, sortedStudents, handleSort } = useStudentSort({
+    students,
   });
-
-  // ソートボタンのクリックイベント
-  const handleSort = (key: SortKey) => {
-    // 別の項目がクリックされたら(時間 -> スコア)選択された項目を「昇順」にする
-    if (sortKey !== key) {
-      setSortKey(key);
-      setSortOrder('asc');
-      return;
-    }
-
-    // 同じ項目がクリックされたら、今の状態を見て昇順 -> 降順 -> ソートなしになるようにする
-    if (sortOrder === undefined) {
-      setSortOrder('asc');
-    } else if (sortOrder === 'asc') {
-      setSortOrder('desc');
-    } else {
-      setSortOrder(undefined);
-      setSortKey(undefined);
-    }
-  };
 
   return (
     <table className="table">
